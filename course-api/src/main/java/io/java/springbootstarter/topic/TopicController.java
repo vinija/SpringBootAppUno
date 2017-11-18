31c4a9b5c214ea76749a13947d8028b13307254a
Vinija Jain <vinija@gmail.com>
2017-11-18 15:49:14 -0800

    @PathVariable

--- course-api/src/main/java/io/java/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/io/java/springbootstarter/topic/TopicController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class TopicController {
 	}
 	
 	@RequestMapping("/topics/{id}")
-	public Topic getTopic(String id) {
+	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
 	}
 	
